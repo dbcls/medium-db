94b66a03f58280ddbdc798b6105edff7bb363d0e
kaname shimizu <kanames911@gmail.com>
2020-03-18 18:22:33 +0900

    gmdb_component_by_gmoid レイアウト

--- src/stanza/gmdb_component_by_gmoid/index.js
+++ src/stanza/gmdb_component_by_gmoid/index.js
@@ -1,128 +1,145 @@
-Stanza(function(stanza, params) {
+Stanza(function(stanza, params){
 
+  const api_url = "http://ep.dbcls.jp/sparqlist/api/";
+  const api_name = "gmdb_component_by_gmoid";
+  const options = makeOptions(params);
+  const data = {};
+  let msg = "";
+  let error = false;
+
+  if(!params["gmo_id"]){
+    error = true;
+    msg = "gmo_id (e.g. GMO_001010) is required";
+  }
+
+  fetch(`${api_url}${api_name}`, options)
+    .then(res => res.json())
+    .then(json => success(json))
+    .catch(e => fail(e))
+    .finally(() => {
+      stanza.render({
+        template: "stanza.html",
+        parameters: {
+          result: data,
+          msg: msg,
+          error: error
+        }
+      });
+    });
+
+  function success(json){
+    data.alt_labels_en = [];
+    data.alt_labels_ja = [];
+    data.super_classes = [];
+    data.sub_classes = [];
+    data.links = [];
+    data.properties = [];
+    data.roles = [];
+    const host = location.host;
+
+
+    data.gmo_id = json.id;
+    data.pref_label = json.pref_label;
+    data.json_label_ja = json.label_ja;
+
+    if(json.alt_labels_en){
+      data.alt_labels_en = json.alt_labels_en.map(str => ({value: str}));
+    }
+
+    if(json.super_classes){
+      data.super_classes = json.super_classes.map(obj => ({
+        gmo_id: obj.gmo_id,
+        uri: obj.uri,
+        label_en: obj.label_en
+      }));
+    }
+
+    if(json.sub_classes){
+      data.sub_classes = json.sub_classes.map(obj => ({
+        gmo_id: obj.gmo_id,
+        uri: obj.uri,
+        label_en: obj.label_en
+      }));
+    }
+
+    if(json.links){
+      for(let i = 0; i < json.links.length; i++){
+        let link = json.links[i];
+        switch(true){
+          case /pccompound/.test(link):
+            data.links.push({
+              "label": "PubChem",
+              "uri": json.links[i]
+            });
+            break;
+          case /wikipedia/.test(link):
+            data.links.push({
+              "label": "Wikipedia",
+              "uri": json.links[i]
+            });
+            break;
+          case /ncicb/.test(link):
+            data.links.push({
+              "label": "NCI Thesaurus",
+              "uri": json.links[i]
+            });
+            break;
+          case /CHEBI/.test(link):
+            data.links.push({
+              "label": "ChEBI",
+              "uri": json.links[i]
+            });
+            break;
+          case /SNOMEDCT/.test(link):
+            data.links.push({
+              "label": "SNOMED-CT",
+              "uri": json.links[i]
+            });
+            break;
+        }
+      }
+    }
+
+    if(json.properties){
+      for(let i = 0; i < json.properties.length; i++){
+        data.properties.push(json.properties[i]);
+        data.properties[i].host = host;
+
+      }
+    }
+
+    if(json.roles){
+      for(let i = 0; i < json.roles.length; i++){
+        data.roles.push(json.roles[i]);
+      }
+    }
+  }
+
+  function fail(e){
+    const gmoID = params["gmo_id"];
+    error = true;
+    msg = `Not found. gmo_id: ${gmoID}`;
+  }
+
+});
+
+
+function makeOptions(params){
   let formBody = [];
 
-  for (let key in params) {
-    if (params[key]) {
-      formBody.push(key + "=" + endocdeURIComponent(params[key]));
+  for(let key in params){
+    if(params[key]){
+      formBody.push(key + "=" + encodeURIComponent(params[key]));
     }
-  };
+  }
 
-  const options = {
+  return {
     method: "POST",
     mode: "cors",
     body: formBody.join("&"),
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
+      "Accept": "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
     }
   };
-
-  let api_url = "http://ep.dbcls.jp/sparqlist/api/";
-  let api_name = "gmdb_component_by_gmoid";
-
-  let q = fetch(api_url + api_name, options).then(res=>res.json());
-
-  console.log(q);
-  q.then(function(json) {
-    let data = {};
-    data.body = {};
-    data.body.alt_labels_en = [];
-    data.body.alt_labels_ja = [];
-    data.body.super_classes = [];
-    data.body.sub_classes = [];
-    data.body.links = [];
-    data.body.properties = [];
-    data.body.roles = [];
-
-    data.body.gmo_id = json.id;
-    data.body.pref_label = json.pref_label;
-    data.body.json_label_ja = json.label_ja;
-
-    if (json.alt_labels_en) {
-      for (let i = 0; i < json.alt_labels_en.length; i++) {
-        if (i == 0) {
-          data.body.alt_labels_en.push({"label": "Alternative label",
-                                        "value": json.alt_labels_en[i]});
-        } else {
-          data.body.alt_labels_en.push({"label": "",
-                                        "value": json.alt_labels_en[i]});
-        }
-      }
-    }
-    if (json.super_classes) { 
-      for (let i = 0; i < json.super_classes.length; i++) {
-        if (i == 0) {
-          data.body.super_classes.push({"label": "Super class",
-                                        "gmo_id": json.super_classes[i].gmo_id,
-                                        "uri": json.super_classes[i].uri,
-                                        "label_en": json.super_classes[i].label_en});
-        } else {
-          data.body.super_classes.push({"label": "",
-                                        "gmo_id": json.super_classes[i].gmo_id,
-                                        "uri": json.super_classes[i].uri,
-                                        "label_en": json.super_classes[i].label_en});
-        }
-      }
-    }
-    if (json.sub_classes) {
-      for (let i = 0; i < json.sub_classes.length; i++) {
-        if (i == 0) {
-          data.body.sub_classes.push({"label": "Sub class",
-                                      "gmo_id": json.sub_classes[i].gmo_id,
-                                      "uri": json.sub_classes[i].uri,
-                                      "label_en": json.sub_classes[i].label_en});
-        } else {
-          data.body.sub_classes.push({"label": "",
-                                      "gmo_id": json.sub_classes[i].gmo_id,
-                                      "uri": json.sub_classes[i].uri,
-                                      "label_en": json.sub_classes[i].label_en});
-        }
-      }
-    }
-    if (json.links) {
-      for (let i = 0; i < json.links.length; i++) {
-        let link = json.links[i];
-        switch(true) {
-        case /pccompound/.test(link):
-          data.body.links.push({"label": "PubChem",
-                                "uri": json.links[i]});
-          break;
-        case /wikipedia/.test(link):
-          data.body.links.push({"label": "Wikipedia",
-                                "uri": json.links[i]});
-          break;
-        case /ncicb/.test(link):
-          data.body.links.push({"label": "NCI Thesaurus",
-                                "uri": json.links[i]});
-          break;
-        case /CHEBI/.test(link):
-          data.body.links.push({"label": "ChEBI",
-                                "uri": json.links[i]});
-          break;
-        case /SNOMEDCT/.test(link):
-          data.body.links.push({"label": "SNOMED-CT",
-                                "uri": json.links[i]});
-          break;
-        }
-      }
-    }
-    if (json.properties) {
-      for (let i = 0; i < json.properties.length; i++) {
-        data.body.properties.push(json.properties[i]);
-      }
-    }
-    if (json.roles) { 
-      for (let i = 0; i < json.roles.length; i++) {
-        data.body.roles.push(json.roles[i]);
-      }
-    }
-
-    stanza.render({
-      template: "stanza.html",
-      parameters: {
-        result: data
-      }
-    });
-  });
-});
+}
--- src/ts/app/ComponentDoc.ts
+++ src/ts/app/ComponentDoc.ts
@@ -5,13 +5,13 @@ import {qs} from "imagelogic-tools/src/dom/qs";
 export class ComponentDoc extends MainDoc {
   constructor() {super();}
 
-  protected  onReady() {
+  protected onReady() {
     super.onReady();
-    const param:string = getPram();
+    const param: string = getPram();
 
-    const info:HTMLElement = qs("#info");
-    const media:HTMLElement = qs("#media");
-    info.setAttribute("query", param);
+    const info: HTMLElement = qs("#info");
+    const media: HTMLElement = qs("#media");
+    info.setAttribute("gmo_id", param);
     media.setAttribute("query", param);
   }
 }
