578b2f85e509d073975b721c7ba1ba874563f5fd
Satoshi Onoda <satoshionoda@gmail.com>
2020-03-19 17:54:43 +0900

    Top用TSできた

--- src/ts/app/TopDoc.ts
+++ src/ts/app/TopDoc.ts
@@ -11,19 +11,69 @@ export class TopDoc extends MainDoc {
 
   protected onReady() {
     const input: HTMLInputElement = qs("#queryInput");
-    const media: HTMLElement = qs("#media");
-    const components: HTMLElement = qs("#components");
-    const organisms: HTMLElement = qs("#organisms");
+    const mediaByIDs: HTMLElement = qs("#mediaByIDs");
+    const mediaByKeywords: HTMLElement = qs("#mediaByKeywords");
+    const componentsByIDs: HTMLElement = qs("#componentsByIDs");
+    const componentsByKeywords: HTMLElement = qs("#componentsByKeywords");
+    const organismsByIDs: HTMLElement = qs("#organismsByIDs");
+    const organismsByKeywords: HTMLElement = qs("#organismsByKeywords");
+
 
     fromEvent(input, "input").pipe(
       map(r => (r.currentTarget as HTMLInputElement).value),
       debounceTime(500),
-    ).subscribe(r => {
-      media.setAttribute("query", r);
-      components.setAttribute("query", r);
-      organisms.setAttribute("query", r);
+      map(r => mapToQuery(r))
+    ).subscribe((r: QueryKeys) => {
+      mediaByIDs.setAttribute("gm_ids", r.gm_ids);
+      componentsByIDs.setAttribute("gmo_ids", r.gmo_ids);
+      organismsByIDs.setAttribute("tax_ids", r.tax_ids);
+      mediaByKeywords.setAttribute("keywords", r.keywords);
+      componentsByKeywords.setAttribute("keywords", r.keywords);
+      organismsByKeywords.setAttribute("keywords", r.keywords);
     });
-
   }
+}
 
+function mapToQuery(original: string): QueryKeys {
+  const arr: string[] = original
+    .split(",")
+    .map(str => str.trim())
+    .filter(str => !!str);
+
+  const gm_ids = arr.filter(str => matchWithGMID(str)).join(",");
+  const gmo_ids = arr.filter(str => matchWithGMOID(str)).join(",");
+  const tax_ids = arr.filter(str => matchWithTAXID(str)).join(",");
+  const ids: string[] = `${gm_ids},${gmo_ids},${tax_ids}`.split(",");
+  const keywords = arr.filter(str => ids.indexOf(str) === -1).join(",");
+
+  return {
+    gm_ids, gmo_ids, tax_ids, keywords
+  };
+}
+
+export function matchWithGMID(str: string): boolean {
+  switch (true) {
+    case str.match(/^JCM_M(\d+)$/) !== null:
+    case str.match(/^NBRC_M(\d+)$/) !== null:
+    case str.match(/^HM(\w+)$/) !== null:
+    case str.match(/^SY(\w+)$/) !== null:
+      return true;
+    default:
+      return false;
+  }
+}
+
+export function matchWithGMOID(str: string): boolean {
+  return str.match(/^(\d){1,7}$/) !== null;
+}
+
+export function matchWithTAXID(str: string): boolean {
+  return str.match(/^GMO_(\d){6}$/) !== null;
+}
+
+interface QueryKeys {
+  gm_ids: string;
+  gmo_ids: string;
+  tax_ids: string;
+  keywords: string;
 }
--- src/ts/test/mytest.spec.ts
+++ src/ts/test/mytest.spec.ts
@@ -1,14 +1,44 @@
 import {MainDoc} from "../app/MainDoc";
+import {matchWithGMID, matchWithGMOID, matchWithTAXID} from "../app/TopDoc";
 
 describe("MainDoc", () => {
-  beforeEach(() => {
-    loadFixtures("products/index.html");
-  });
   it("should be created", () => {
     expect(new MainDoc()).toBeTruthy();
   });
-  it("should find fixture", () => {
-    const result = $("h1").text();
-    expect(result).toBe("HELLO WORLD");
+});
+
+
+describe("top", () => {
+  it("should match with gm_id", () => {
+    // ^JCM_M(\d+)$
+    expect(matchWithGMID("JCM_M101025")).toBe(true);
+    expect(matchWithGMID("JCM_MX101025")).toBe(false);
+    expect(matchWithGMID("JCM_M101025A")).toBe(false);
+    //
+    // ^NBRC_M(\d+)$
+    expect(matchWithGMID("NBRC_M981455")).toBe(true);
+    expect(matchWithGMID("NBRC_ME981455")).toBe(false);
+    expect(matchWithGMID("NBRC_M981455X")).toBe(false);
+    //
+    // ^HM(\w+)$
+    expect(matchWithGMID("HM981455")).toBe(true);
+    expect(matchWithGMID("HM_981455")).toBe(true);
+    expect(matchWithGMID("HM")).toBe(false);
+    //
+    // ^SY(\w+)$
+    expect(matchWithGMID("SY981455")).toBe(true);
+    expect(matchWithGMID("SY_981455")).toBe(true);
+    expect(matchWithGMID("SY")).toBe(false);
+  });
+  it("should match with gmo_id", () => {
+    expect(matchWithGMOID("102")).toBe(true);
+    expect(matchWithGMOID("SY123456")).toBe(false);
+    expect(matchWithGMOID("00112358")).toBe(false);
+  });
+  it("should match with tax_id", () => {
+    expect(matchWithTAXID("GMO_001020")).toBe(true);
+    expect(matchWithTAXID("GMO_0010209")).toBe(false);
+    expect(matchWithTAXID("GMO_A001020")).toBe(false);
+    expect(matchWithTAXID("GMOW_001020")).toBe(false);
   });
 });
