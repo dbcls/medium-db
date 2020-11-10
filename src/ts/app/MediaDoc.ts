607e0f5af52b3c5f40a942e4070717d8225d0362
Satoshi Onoda <satoshionoda@gmail.com>
2020-11-10 10:13:24 +0900

    類似培地組み込み

--- src/ts/app/MediaDoc.ts
+++ src/ts/app/MediaDoc.ts
@@ -15,6 +15,8 @@ export class MediaDoc extends MainDoc {
     info.setAttribute("gm_id", param);
     organisms.setAttribute("gm_id", param);
     GMID.innerText = param;
-
+    const similarList: HTMLElement = qs("#similar");
+    similarList.setAttribute("api_url", `http://growthmedium.org/sparqlist/api/gmdb_list_similar_media_by_gmid?gm_id=${param}`)
+    similarList.setAttribute("title", `Similar Growth Media of ${param}`);
   }
 }
