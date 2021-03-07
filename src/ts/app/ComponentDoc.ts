fb1fe557daa24822d6066f9af511fb5e71a6e560
Satoshi Onoda <satoshionoda@gmail.com>
2021-03-07 14:16:26 +0900

    Component / Stats

--- src/ts/app/ComponentDoc.ts
+++ src/ts/app/ComponentDoc.ts
@@ -1,23 +1,28 @@
-import {MainDoc} from "./MainDoc";
-import {getPram} from "./utils";
-import {qs} from "imagelogic-tools/src/dom/qs";
+import { MainDoc } from "./MainDoc";
+import { getPram } from "./utils";
+import { qs } from "imagelogic-tools/src/dom/qs";
 
 export class ComponentDoc extends MainDoc {
-  constructor() {super();}
+  constructor() {
+    super();
+  }
 
   protected onReady() {
     super.onReady();
-    const param: string = getPram();
-
-    const info: HTMLElement = qs("#info");
-    info.setAttribute("gmo_id", param);
-    //
-    const mediaWrapper: HTMLElement = qs("#media");
-    const mediaStanza: HTMLElement = document.createElement("togostanza-gmdb_meta_list");
-    mediaStanza.setAttribute("limit", "10");
-    mediaStanza.setAttribute("column_names", "true");
-    mediaStanza.setAttribute("api_url", `http://growthmedium.org/sparqlist/api/gmdb_media_by_gmoid?gmo_id=${param}`);
-    mediaStanza.setAttribute("title", `Media with ${param}`);
-    mediaWrapper.append(mediaStanza);
+    setup();
   }
 }
+
+const setup = () => {
+  const param: string = getPram();
+
+  const info: HTMLElement = qs("#info");
+  info.setAttribute("gmo_id", param);
+  //
+  const mediaStanza: HTMLElement = qs("#media");
+  mediaStanza.setAttribute(
+    "api_url",
+    `http://growthmedium.org/sparqlist/api/gmdb_media_by_gmoid?gmo_ids=${param}`
+  );
+  mediaStanza.setAttribute("title", `Media with ${param}`);
+};
