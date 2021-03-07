ebf8d20d7f6c9e9f2a7224f39dec87dcdcde8c83
Satoshi Onoda <satoshionoda@gmail.com>
2021-03-07 10:25:13 +0900

    Organism ざっくりと

--- src/ts/app/MediaDoc.ts
+++ src/ts/app/MediaDoc.ts
@@ -11,43 +11,6 @@ export class MediaDoc extends MainDoc {
     super.onReady();
     setup();
   }
-
-  protected temp() {
-    const param: string = getPram();
-
-    const info: HTMLElement = qs("#info");
-    info.setAttribute("gm_id", param);
-
-    const similarWrapper: HTMLElement = qs("#similar");
-    const similarStanza: HTMLElement = document.createElement(
-      "togostanza-gmdb_meta_list"
-    );
-    similarStanza.setAttribute("limit", "10");
-    similarStanza.setAttribute("column_names", "true");
-    similarStanza.setAttribute(
-      "api_url",
-      `http://growthmedium.org/sparqlist/api/gmdb_list_similar_media_by_gmid?gm_id=${param}`
-    );
-    similarStanza.setAttribute("title", `Similar Growth Media of ${param}`);
-    similarWrapper.append(similarStanza);
-
-    const organismsWrapper: HTMLElement = qs("#organisms");
-    const organismsStanza: HTMLElement = document.createElement(
-      "togostanza-gmdb_meta_list"
-    );
-    organismsStanza.setAttribute("limit", "10");
-    organismsStanza.setAttribute("column_names", "true");
-    organismsStanza.setAttribute(
-      "api_url",
-      `http://growthmedium.org/sparqlist/api/gmdb_organisms_by_gmid?gm_id=${param}`
-    );
-    organismsStanza.setAttribute("title", `Organisms cultured in ${param}`);
-    setTimeout(() => {
-      organismsWrapper.append(organismsStanza);
-    }, 500);
-
-    // const similarList: HTMLElement = qs("#similar");
-  }
 }
 
 const setup = () => {
--- src/ts/app/OrganismDoc.ts
+++ src/ts/app/OrganismDoc.ts
@@ -1,24 +1,42 @@
-import {MainDoc} from "./MainDoc";
-import {getPram} from "./utils";
-import {qs} from "imagelogic-tools/src/dom/qs";
+import { MainDoc } from "./MainDoc";
+import { getPram } from "./utils";
+import { qs } from "imagelogic-tools/src/dom/qs";
 
 export class OrganismDoc extends MainDoc {
-  constructor() {super();}
+  constructor() {
+    super();
+  }
 
   protected onReady() {
     super.onReady();
-    const param: string = getPram();
-
-    const info: HTMLElement = qs("#info");
-    info.setAttribute("tax_id", param);
+    setup();
+    // const param: string = getPram();
     //
-    const mediaWrapper: HTMLElement = qs("#media");
-    const mediaStanza: HTMLElement = document.createElement("togostanza-gmdb_meta_list");
-    mediaStanza.setAttribute("limit", "10");
-    mediaStanza.setAttribute("column_names", "true");
-    mediaStanza.setAttribute("api_url", `http://growthmedium.org/sparqlist/api/gmdb_media_by_taxid?gmo_ids=${param}`);
-    mediaStanza.setAttribute("title", `Media with ${param}`);
-    mediaWrapper.append(mediaStanza);
+    // const info: HTMLElement = qs("#info");
+    // info.setAttribute("tax_id", param);
+    // //
+    // const mediaWrapper: HTMLElement = qs("#media");
+    // const mediaStanza: HTMLElement = document.createElement("togostanza-gmdb_meta_list");
+    // mediaStanza.setAttribute("limit", "10");
+    // mediaStanza.setAttribute("column_names", "true");
+    // mediaStanza.setAttribute("api_url", `http://growthmedium.org/sparqlist/api/gmdb_media_by_taxid?gmo_ids=${param}`);
+    // mediaStanza.setAttribute("title", `Media with ${param}`);
+    // mediaWrapper.append(mediaStanza);
   }
 }
 
+const setup = () => {
+  const id: string = getPram();
+  const info: HTMLElement = qs("#info");
+  info.setAttribute("tax_id", id);
+
+  const phenotype: HTMLElement = qs("#phenotype");
+  phenotype.setAttribute("tax_id", id);
+
+  const media: HTMLElement = qs("#media");
+  media.setAttribute(
+    "api_url",
+    `http://growthmedium.org/sparqlist/api/gmdb_media_by_taxid?tax_id=${id}`
+  );
+  media.setAttribute("title", `Media with ${id}`);
+};
