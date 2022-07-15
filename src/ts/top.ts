0f9c960d59d72f50795e296eab778d24a36ca97f
satoshionoda <satoshionoda@gmail.com>
2022-07-15 17:45:03 +0900

    TopからTaxonフィルター削除

--- src/ts/top.ts
+++ src/ts/top.ts
@@ -1,12 +1,11 @@
 import { EVENT_READY } from "./app/Consts";
 import { setUpFreeSearch } from "./app/components/FreeSearch";
-import { setUpTaxonFilter } from "./app/components/TaxonFilter";
 import { setUpStats } from "./app/components/setUpStats";
 
 (() => {
   document.addEventListener(EVENT_READY, () => {
     setUpFreeSearch();
-    setUpTaxonFilter();
+    // setUpTaxonFilter();
     setUpStats();
   });
 })();
