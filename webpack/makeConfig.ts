0a730e1cb6cefd07366b26b98d0ee0804e58984c
satoshionoda <satoshionoda@gmail.com>
2022-07-15 17:44:34 +0900

    Webpack設定変更

--- webpack/makeConfig.ts
+++ webpack/makeConfig.ts
@@ -223,6 +223,11 @@ const makeLessLoadRule = (): RuleSetRule => {
       },
       {
         loader: "less-loader",
+        options: {
+          lessOptions: {
+            relativeUrls: false,
+          },
+        },
       },
     ],
   };
