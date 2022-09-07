f93cda8fabf95c470389915f1ec404418b4ef53f
satoshionoda <satoshionoda@gmail.com>
2022-09-07 10:17:43 +0900

    compare  で not foundが出るように

--- src/ts/compare.ts
+++ src/ts/compare.ts
@@ -1,5 +1,5 @@
 import { EVENT_CLICK, EVENT_READY } from "./app/Consts";
-import { qs } from "imagelogic-tools/src/dom/qs";
+import { Nullable, qs } from "yohak-tools";
 
 (() => {
   document.addEventListener(EVENT_READY, () => {
@@ -11,13 +11,11 @@ import { qs } from "imagelogic-tools/src/dom/qs";
     const button = qs("#compareBtn");
     const url = new URL(location.href);
     const queriedGmIds = url.searchParams.get("gm_ids");
+    let queriedData: string[] = [];
     //
     const execute = () => {
-      const value = ids.value
-        .split(",")
-        // .reverse()
-        .map((str) => str.trim())
-        .join(",");
+      queriedData = ids.value.split(",").map((str) => str.trim());
+      const value = queriedData.join(",");
       table.setAttribute("gm_ids", value);
       table.style.display = "block";
       //
@@ -35,5 +33,38 @@ import { qs } from "imagelogic-tools/src/dom/qs";
     //
 
     button.addEventListener(EVENT_CLICK, () => execute());
+    document.addEventListener("STANZA_ON_QUERY_DATA", (e: CustomEvent) => {
+      queriedData = (e.detail as string[]).filter((str) => str !== "");
+    });
+    document.addEventListener("STANZA_ON_LOAD_DATA", (e: CustomEvent) => {
+      console.log("ONLOAD", e);
+      const response: MediaAlignmentTableResponse = e.detail;
+      const loadedMedia: string[] = response.media.map((m) => m.gm_id);
+      //
+      const notFound = queriedData.filter((str) => !loadedMedia.includes(str));
+      const errorMsg = notFound.length
+        ? `Not found: ${notFound.join(", ")}`
+        : "";
+      qs("#errorMsg").textContent = errorMsg;
+    });
   };
 })();
+
+export type MediaAlignmentTableResponse = {
+  media: {
+    gm_id: string;
+    name: string;
+    components: string[];
+    organisms: string[];
+  }[];
+  organisms: {
+    tax_id: string;
+    name: string;
+  }[];
+  components: {
+    gmo_id: string;
+    name: string;
+    parent: Nullable<string>;
+    function: Nullable<string>;
+  }[];
+};
